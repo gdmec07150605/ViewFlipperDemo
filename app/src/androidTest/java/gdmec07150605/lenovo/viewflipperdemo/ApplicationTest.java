1b99422ab92cab3cf49809b1db10ae4067df9be7
unknown <782230913@qq.com>
2016-12-30 15:48:06 +0800

    Initial commit

---
+++ app/src/androidTest/java/gdmec07150605/lenovo/viewflipperdemo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package gdmec07150605.lenovo.viewflipperdemo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/gdmec07150605/lenovo/viewflipperdemo/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package gdmec07150605.lenovo.viewflipperdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
